bb0dbfef1bab90676fefc5f2ba417b1160bbab99
ts-milen.georgiev <ts-milen.georgiev@rakuten.com>
2020-10-14 20:41:46 +0900

    Refactoring.

--- bin/markdown2confluence.js
+++ bin/markdown2confluence.js
@@ -13,8 +13,13 @@ fs.readFile(path.resolve(process.cwd(), filename), function(err, buffer) {
 
     // We want to remove widdershins metadata
     const commentEnd = buffer.indexOf('-->', 0);
-
+    
     // Generate the confluence wiki markup
     const confluenceMarkup = md2conflu(buffer.slice(commentEnd + 3, buffer.length) + '')
-    fs.writeFileSync(path.resolve(process.cwd(), outputFileName), confluenceMarkup)
+    
+    if(outputFileName){
+        fs.writeFileSync(path.resolve(process.cwd(), outputFileName), confluenceMarkup)
+    }
+
+    return confluenceMarkup;
 })
--- index.js
+++ index.js
@@ -1,104 +1,355 @@
 var marked = require('marked')
 var _ = require('min-util')
 var qs = require('min-qs')
-var inlineLexer = marked.inlineLexer
-
-module.exports = exports = markdown2confluence
 
 // https://roundcorner.atlassian.net/secure/WikiRendererHelpAction.jspa?section=all
 // https://confluence.atlassian.com/display/DOC/Confluence+Wiki+Markup
 // http://blogs.atlassian.com/2011/11/why-we-removed-wiki-markup-editor-in-confluence-4/
 
-var MAX_CODE_LINE = 20
-
 function Renderer() {}
 
 var rawRenderer = marked.Renderer
 
-var langArr = 'actionscript3 bash csharp coldfusion cpp css delphi diff erlang groovy java javafx javascript perl php none powershell python ruby scala sql vb html/xml'.split(/\s+/)
-var langMap = {
-	shell: 'bash', 
-	html: 'html', 
-	xml: 'xml'
-}
-for (var i = 0, x; x = langArr[i++];) {
-	langMap[x] = x
-}
+// https://confluence.atlassian.com/doc/code-block-macro-139390.html
+var defaultLanguageMap = require('./defaultLanguageMap.json')
 
 _.extend(Renderer.prototype, rawRenderer.prototype, {
+	/**
+     * Simple text.
+     *
+     * @param {string} text
+     * @return {string}
+     */
 	text: function (text){
 		return text;
 	},
+	/**
+     * A paragraph of text.
+     *
+     * @param {string} text
+     * @return {string}
+     */
 	paragraph: function(text) {
-		return text + '\n\n'
+		return `${text}\n\n`;
 	}
-	, html: function(html) {
-		html = html.replace(/<([\w0-9]+)[\s\w="-_]*>([\w\s.!?\\-]*)<\/\1>/gi, '$1. $2')
-		return html
+	/**
+     * Embedded HTML.
+     *
+     *   <h1>My text</h1>
+     *
+     * turns into
+     *
+     *   h1. My text
+     *
+     * @param {string} text
+     * @return {string}
+     */
+	, html: function(text) {
+		return html.replace(/<([\w0-9]+)[\s\w="-_]*>([\w\s.!?\\-]*)<\/\1>/gi, '$1. $2')
 	}
-	, heading: function(text, level, raw) {
-		return 'h' + level + '. ' + text + '\n\n'
+	/**
+     * Headings 1 through 6.
+     *
+     *   Heading 1
+     *   =========
+     *
+     *   # Heading 1 alternate
+     *
+     *   ###### Heading 6
+     *
+     * turns into
+     *
+     *   h1. Heading 1
+     *
+     *   h1. Heading 1 alternate
+     *
+     *   h6. Heading 6
+     *
+     * @param {string} text
+     * @param {number} level
+     * @return {string}
+     */
+	, heading: function(text, level) {
+		return `h${level}. ${text}\n\n`;
 	}
+	/**
+     * Creates strong text.
+     *
+     *   This is typically **bolded**.
+     *
+     * becomes
+     *
+     *   This is typically *bolded*.
+     *
+     * @param {string} text
+     * @return {string}
+     */
 	, strong: function(text) {
-		return '*' + text + '*'
+		return `*${text}*`;
 	}
+	/**
+     * Emphasis.
+     *
+     *   Typically this is *italicized* text.
+     *
+     * turns into
+     *
+     *   Typically this is _italicized_ text.
+     *
+     * @param {string} text
+     * @return {string}
+     */
 	, em: function(text) {
-		return '_' + text + '_'
+		return `_${text}_`;
 	}
+	/**
+     * Strikethrough.
+     *
+     *   Supported ~~everywhere~~ in GFM only.
+     *
+     * turns into
+     *
+     *   Supported -everywhere- in GFM only.
+     *
+     * @param {string} text
+     * @return {string}
+     */
 	, del: function(text) {
-		return '-' + text + '-'
+		return `-${text}-`;
 	}
+	/**
+     * Inline code.
+     *
+     *   Text that has statements, like `a = true` or similar.
+     *
+     * turns into
+     *
+     *   Text that has statements, like {{a = true}} or similar.
+     *
+     * Be wary. This converts wrong: "Look at `~/file1` or `~/file2`"
+     * Confluence thinks it is subscript and converts the markup into
+     * "Look at <code><sub>/file1</code> or <code></sub>/file2</code>".
+     * That's why some characters need to be escaped.
+     *
+     * @param {string} text
+     * @return {string}
+     */
 	, codespan: function(text) {
-		return '{{' + text + '}}'
+		text = text.split(/(&[^;]*;)/).map((match, index) => {
+            // These are the delimeters.
+            if (index % 2) {
+                return match;
+            }
+
+            return match.replace(/[^a-zA-Z0-9 ]/g, (badchar) => {
+                return `&#${badchar[0].charCodeAt(0)};`;
+            });
+        });
+
+        return `{{${text.join("")}}}`;
 	}
-	, blockquote: function(quote) {
-		return '{quote}' + quote + '{quote}'
+	/**
+     * Blockquote.
+     *
+     *   > This is a blockquote.
+     *
+     * is changed into
+     *
+     *   {quote}
+     *   This is a blockquote.
+     *   {quote}
+     *
+     * @param {string} text
+     * @return {string}
+     */
+	, blockquote: function(text) {
+		return `{quote}\n${text.trim()}\n{quote}\n\n`;
 	}
+	/**
+     * A line break. Supposedly if you have a line with 2 or more spaces
+     * followed by a line that doesn't have whitespace, then it turns into
+     * this element. I'm failing to reproduce that scenario.
+     *
+     * @return {string}
+     */
 	, br: function() {
 		return '\n'
 	}
+	/**
+     * Horizontal rule.
+     *
+     *   ---
+     *
+     * turns into
+     *
+     *   ----
+     *
+     * @return {string}
+     */
 	, hr: function() {
-		return '----'
+		return '----\n\n'
 	}
+	/**
+     * Link to another resource.
+     *
+     *   [Home](/)
+     *   [Home](/ "some title")
+     *
+     * turns into
+     *
+     *   [Home|/]
+     *   [some title|/]
+     *
+     * @param {string} href
+     * @param {string} title
+     * @param {string} text
+     * @return {string}
+     */
 	, link: function(href, title, text) {
-		var arr = [href]
-		if (text) {
-			arr.unshift(text)
-		}
-		return '[' + arr.join('|') + ']'
-	}
-	, list: function(body, ordered) {
-		var arr = _.filter(_.trim(body).split('\n'), function(line) {
-			return line
-		})
-		var type = ordered ? '#' : '*'
-		return _.map(arr, function(line) {
-			return type + ' ' + line
-		}).join('\n') + '\n\n'
+		// Sadly, one must choose if the link's title should be displayed
+        // or the linked text should be displayed. We picked the linked text.
+        text = text || title;
 
+        if (text) {
+            text += "|";
+        }
+
+		return `[${text}${href}]`;
 	}
-	, listitem: function(body, ordered) {
-		return body + '\n'
+	/**
+     * Converts a list.
+     *
+     *     # ordered
+     *         * unordered
+     *
+     * becomes
+     *
+     *     # ordered
+     *     #* unordered
+     *
+     * Note: This adds an extra "\r" before the list in order to cope
+     * with nested lists better. When there's a "\r" in a nested list, it
+     * is translated into a "\n". When the "\r" is left in the converted
+     * result then it is removed.
+     *
+     * @param {string} text
+     * @param {boolean} ordered
+     * @return {string}
+     */
+	, list: function(text, ordered) {
+		text = text.trim();
+
+        if (ordered) {
+            text = text.replace(/^\*/gm, "#");
+        }
+
+        return `\r${text}\n\n`;
 	}
-	, image: function(href, title, text) {
-		return '!' + href + '!'
+	/**
+     * Changes a list item. Always marks it as an unordered list, but
+     * list() will change it back.
+     *
+     * @param {string} text
+     * @return {string}
+     */
+	, listitem: function(text) {
+		// If a list item has a nested list, it will have a "\r" in the
+        // text. Turn that "\r" into "\n" but trim out other whitespace
+        // from the list.
+        text = text.replace(/\s*$/, "").replace(/\r/g, "\n");
+
+        // Convert newlines followed by a # or a * into sub-list items
+        text = text.replace(/\n([*#])/g, "\n*$1");
+
+        return `* ${text}\n`;
 	}
+	/**
+     * An embedded image.
+     *
+     *   ![alt-text](image-url)
+     *
+     * is changed into
+     *
+     *   !image-url!
+     *
+     * Markdown supports alt text and titles. Confluence does not.
+     *
+     * @param {string} href
+     * @return {string}
+     */
+	, image: function(href) {
+		return `!${href}!`;
+	}
+	/**
+     * Renders a table. Most of the work is done in tablecell.
+     *
+     * @param {string} header
+     * @param {string} body
+     * @return {string}
+     */
 	, table: function(header, body) {
-		return header + body + '\n'
+		return `${header}${body}\n`;
 	}
-	, tablerow: function(content, flags) {
-		return content + '\n'
+	/**
+     * Converts a table row. Most of the work is done in tablecell, however
+     * that can't tell if the cell is at the end of a row or not. Get the
+     * first cell's leading boundary and remove the double-boundary marks.
+     *
+     * @param {string} text
+     * @return {string}
+     */
+	, tablerow: function(text) {
+		var boundary;
+
+        boundary = text.match(/^\|*/);
+
+        if (boundary) {
+            boundary = boundary[0];
+        } else {
+            boundary = "|";
+        }
+
+        return `${text}${boundary}\n`;
 	}
-	, tablecell: function(content, flags) {
-		var type = flags.header ? '||' : '|'
-		return type + content
+	/**
+     * Converts a table cell. When this is a header, the cell is prefixed
+     * with two bars instead of one.
+     *
+     * @param {string} text
+     * @param {Object} flags
+     * @return {string}
+     */
+	, tablecell: function(text, flags) {
+		var boundary;
+
+        if (flags.header) {
+            boundary = "||";
+        } else {
+            boundary = "|";
+        }
+
+        return `${boundary}${text}`;
 	}
-	, code: function(code, lang) {
-		// {code:language=java|borderStyle=solid|theme=RDark|linenumbers=true|collapse=true}
-		if(lang) {
-			lang = lang.toLowerCase()
-		}
-		lang = langMap[lang] || 'none'
+	/**
+     * Code block.
+     *
+     *   ```js
+     *   // JavaScript code
+     *   ```
+     *
+     * is changed into
+     *
+     *   {code:language=javascript|borderStyle=solid|theme=RDark|linenumbers=true|collapse=true}
+     *   // JavaScript code
+     *   {code}
+     *
+     * @param {string} text
+     * @param {string} lang
+     * @return {string}
+     */
+	, code: function(text, lang) {
+		lang = defaultLanguageMap[(lang ?? "").toLowerCase()];
+
 		var param = {
 			title: lang,
 			language: lang,
@@ -107,18 +358,17 @@ _.extend(Renderer.prototype, rawRenderer.prototype, {
 			linenumbers: true,
 			collapse: true
 		}
-		// var lineCount = _.split(code, '\n').length
-		// if (lineCount > MAX_CODE_LINE) {
-		// 	// code is too long
-		// 	param.collapse = true
-		// }
+
 		param = qs.stringify(param, '|', '=')
-		return '{code:' + param + '}\n' + code + '\n{code}\n\n'
+		return `{code${param}}\n${text}\n{code}\n\n`;
+
 	}
 })
 
 var renderer = new Renderer()
 
-function markdown2confluence(markdown) {
+markdown2confluence = (markdown, options) => {
 	return marked(markdown, {renderer: renderer})
-}
+};
+
+module.exports = markdown2confluence;

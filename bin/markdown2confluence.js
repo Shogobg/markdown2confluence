ded78db1a9f593b8af82612060640da2bb4ad677
ts-milen.georgiev <ts-milen.georgiev@rakuten.com>
2020-10-18 13:54:06 +0900

    Fix an issue when there is no comment in the MD

--- bin/markdown2confluence.js
+++ bin/markdown2confluence.js
@@ -12,11 +12,13 @@ fs.readFile(path.resolve(process.cwd(), filename), (err, buffer) => {
   assert(!err, 'read file ' + filename + ' error!');
 
   // We want to remove widdershins metadata
-  const commentEnd = buffer.indexOf('-->', 0);
+  let commentEnd = buffer.indexOf('-->', 0);
+
+  commentEnd = commentEnd > 0 ? (commentEnd += 3) : 0;
 
   // Generate the confluence wiki markup
   const confluenceMarkup = md2conflu(
-    buffer.slice(commentEnd + 3, buffer.length) + '',
+    buffer.slice(commentEnd, buffer.length) + '',
   );
 
   if (outputFileName) {
